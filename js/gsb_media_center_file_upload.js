a27220afc64cdfff3e2ca16e249e589ea373c848
Dumitru Vozian <dumitru.vozian@wearepropeople.com>
2013-05-22 10:44:28 -0700

    Task #WP-573: added js files that activate tabs after clicking previous button

---
+++ js/gsb_media_center_file_upload.js
@@ -0,0 +1,4 @@
+Drupal.behaviors.gsb_media_center_file_upload = {
+  attach: function () {
+    jQuery('#media-browser-tabset').tabs('option', 'selected', 'media-tab-gsb_media_center_document');
+}};
---
+++ js/gsb_media_center_video_upload.js
@@ -0,0 +1,4 @@
+Drupal.behaviors.gsb_media_center_video_upload = {
+  attach: function () {
+    jQuery('#media-browser-tabset').tabs('option', 'selected', 'media-tab-gsb_media_center_video');
+}};
